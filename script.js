2e8e62984d89a8dafdd4f9e9331c6fcfd6e71670
GiaJames <153130536+GiaJames@users.noreply.github.com>
2023-12-06 15:24:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var coll = document.getElementsByClassName("instructions");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+let intervalid;
+function called(){
+console.log("Hello")
+}
+
+function startFunction(time){
+intervalid=setInterval(called,1000);
+}
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get users answer from div
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+ 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+
+
